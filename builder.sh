d0d9978732359970df1014997bfafa30575cd2d7
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-08-05 14:20:14 +0600

    builder: Switch to `default-QTI` branch
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- builder.sh
+++ builder.sh
@@ -27,7 +27,7 @@ gut https://github.com/Tashar02/AnyKernel3.git AnyKernel3
 
 ############################### Setup Kernel ###############################
 
-gut https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b rebase Kernel
+gut https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b default-QTI Kernel
 
 ############################################################################
 
