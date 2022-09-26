ceddf2297dc929f7d20c35a39757a5fc45baffb4
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-09-27 01:29:41 +0600

    builder: Install zstd v1.5.2
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- builder.sh
+++ builder.sh
@@ -8,6 +8,9 @@ gut() {
 
 ############################################################################
 
+wget https://github.com/dakkshesh07/zstd-pkgbuild/releases/download/1.5.2-8/zstd-1.5.2-8-x86_64.pkg.tar.zst
+pacman -U --noconfirm zstd-1.5.2-8-x86_64.pkg.tar.zst
+
 ############################# Setup Toolchains #############################
 
 mkdir toolchains
