ef1f3b148a9a72262cec0aebb1a6b172d46da0b1
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-10-04 14:10:28 +0600

    builder: Cleanup and reformat using shfmt
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- builder.sh
+++ builder.sh
@@ -1,45 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-###############################   MISC   ###################################
-
+# Helper function for cloning
 gut() {
 	git clone --depth=1 -q $@
 }
 
-############################################################################
-
+# Install zstd
 wget https://github.com/dakkshesh07/zstd-pkgbuild/releases/download/1.5.2-8/zstd-1.5.2-8-x86_64.pkg.tar.zst
 pacman -U --noconfirm zstd-1.5.2-8-x86_64.pkg.tar.zst
 
-############################# Setup Toolchains #############################
-
+# Toolchains directory
 mkdir toolchains
 
+# Clone GCC
 #gut https://github.com/cyberknight777/gcc-arm64.git -b master toolchains/gcc64
 gut https://github.com/mvaisakh/gcc-arm.git -b gcc-master toolchains/gcc32
 
+# Clone CLANG
 mkdir toolchains/clang && cd toolchains/clang
-bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S=latest
+bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S
 cd ../..
 
-############################################################################
-
-############################## Setup AnyKernel #############################
-
+# Clone AnyKernel3
 gut https://github.com/Tashar02/AnyKernel3.git AnyKernel3
 
-############################################################################
-
-############################### Setup Kernel ###############################
-
+# Clone Kernel Source
 gut https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b test Kernel
 
-############################################################################
-
-############################## Setup Scripts ###############################
-
+# Setup Scripts
 mv scarlet.sh Kernel/scarlet.sh
 cd Kernel
-bash scarlet.sh clang
 
-############################################################################
+# Compile the kernel using CLANG
+bash scarlet.sh clang
--- scarlet.sh
+++ scarlet.sh
@@ -1,36 +1,43 @@
 #!/usr/bin/env bash
 # Copyright (c) 2021-2022, Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
-# Revision: 06-06-2022 V7.0
+# Revision: 04-10-2022 V7.1
 
-# USER
-	USER='Tashar'
-	HOST='Cirrus'
-	TOKEN=${token}
-	CHATID=${chat_id}
-	BOT_MSG_URL="https://api.telegram.org/bot$TOKEN/sendMessage"
-	BOT_BUILD_URL="https://api.telegram.org/bot$TOKEN/sendDocument"
-	COMPILER="$1"
+# Colors
+R='\033[1;31m'
+G='\033[1;32m'
+Y='\033[1;33m'
+B='\033[1;34m'
+W='\033[1;37m'
 
-# SCRIPT CONFIG
-	SILENCE='0'
-	SDFCF='1'
-	BUILD='clean'
+# User infomation
+USER='Tashar'
+HOST='Cirrus'
+TOKEN=${token}
+CHATID=${chat_id}
+BOT_MSG_URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+BOT_BUILD_URL="https://api.telegram.org/bot$TOKEN/sendDocument"
+COMPILER="$1"
 
-# DEVICE CONFIG
-	NAME='Mi A2 / 6X'
-	DEVICE='wayne'
-	DEVICE2='jasmine'
-	CAM_LIB='1'
-	HAPTICS='1'
+# Script configuration
+SILENCE='0'
+SDFCF='1'
+BUILD='clean'
 
-# PATH
-	KERNEL_DIR=`pwd`
-	TOOLCHAIN="$KERNEL_DIR/../toolchains"
-	ZIP_DIR="$KERNEL_DIR/../AnyKernel3"
-	AKSH="$ZIP_DIR/anykernel.sh"
-	cd $KERNEL_DIR
+# Device configuration
+NAME='Mi A2 / 6X'
+DEVICE='wayne'
+DEVICE2='jasmine'
+CAM_LIB='1'
+HAPTICS='1'
 
-# DEFCONFIG
+# Paths
+KERNEL_DIR=$(pwd)
+TOOLCHAIN="$KERNEL_DIR/../toolchains"
+ZIP_DIR="$KERNEL_DIR/../AnyKernel3"
+AKSH="$ZIP_DIR/anykernel.sh"
+cd $KERNEL_DIR
+
+# Defconfig selection
 if [[ "$CAM_LIB" == "1" ]]; then
 	DFCF="vendor/${DEVICE}-perf_defconfig"
 elif [[ "$CAM_LIB" == "2" ]]; then
@@ -38,15 +45,9 @@ elif [[ "$CAM_LIB" == "2" ]]; then
 elif [[ "$CAM_LIB" == "3" ]]; then
 	DFCF="vendor/${DEVICE}-oss-perf_defconfig"
 fi
-	CONFIG="$KERNEL_DIR/arch/arm64/configs/$DFCF"
+CONFIG="$KERNEL_DIR/arch/arm64/configs/$DFCF"
 
-# COLORS
-	R='\033[1;31m'
-	G='\033[1;32m'
-	Y='\033[1;33m'
-	B='\033[1;34m'
-	W='\033[1;37m'
-
+# Helper function to print error message
 error() {
 	echo -e ""
 	echo -e "$R Error! $Y$1"
@@ -54,6 +55,7 @@ error() {
 	exit 1
 }
 
+# User and hostname detection
 if [[ "$USER" == "" ]]; then
 	clear
 	echo -ne "$G \n User not defined! Manual input required :$W "
@@ -64,6 +66,8 @@ if [[ "$HOST" == "" ]]; then
 	echo -ne "$G \n Host not defined! Manual input required :$W "
 	read -r HOST
 fi
+
+# Silencing verbose logging
 if [[ "$SILENCE" == "1" ]]; then
 	FLAG=-s
 fi
@@ -87,47 +91,47 @@ pass() {
 		clear
 		error 'Value not recognized'
 	fi
-		CC_32="$TOOLCHAIN/gcc32/bin/arm-eabi-"
-		CC_COMPAT="$TOOLCHAIN/gcc32/bin/arm-eabi-gcc"
-		build
+	CC_32="$TOOLCHAIN/gcc32/bin/arm-eabi-"
+	CC_COMPAT="$TOOLCHAIN/gcc32/bin/arm-eabi-gcc"
+	build
 }
 export PATH=$C_PATH/bin:$PATH
 
-# Compilation
+# Function to pass compilation flags
 muke() {
-	make O=work $CFLAG ARCH=arm64 $FLAG	     \
-			CC=$CC                           \
-			LLVM=1                           \
-			LLVM_IAS=1                       \
-			PYTHON=python3                   \
-			KBUILD_BUILD_USER=$USER          \
-			KBUILD_BUILD_HOST=$HOST          \
-			AS=llvm-as                       \
-			AR=llvm-ar                       \
-			NM=llvm-nm                       \
-			LD=ld.lld                        \
-			STRIP=llvm-strip                 \
-			OBJCOPY=llvm-objcopy             \
-			OBJDUMP=llvm-objdump             \
-			OBJSIZE=llvm-objsize             \
-			HOSTLD=ld.lld                    \
-			HOSTCC=$HOSTCC                   \
-			HOSTCXX=$HOSTCXX                 \
-			HOSTAR=llvm-ar                   \
-			PATH=$C_PATH/bin:$PATH           \
-			CROSS_COMPILE=$CC_64             \
-			CC_COMPAT=$CC_COMPAT             \
-			CROSS_COMPILE_COMPAT=$CC_32      \
-			LD_LIBRARY_PATH=$C_PATH/lib:$LD_LIBRARY_PATH \
-			2>&1 | tee log.txt
+	make O=work $CFLAG ARCH=arm64 $FLAG \
+		CC=$CC \
+		LLVM=1 \
+		LLVM_IAS=1 \
+		PYTHON=python3 \
+		KBUILD_BUILD_USER=$USER \
+		KBUILD_BUILD_HOST=$HOST \
+		AS=llvm-as \
+		AR=llvm-ar \
+		NM=llvm-nm \
+		LD=ld.lld \
+		STRIP=llvm-strip \
+		OBJCOPY=llvm-objcopy \
+		OBJDUMP=llvm-objdump \
+		OBJSIZE=llvm-objsize \
+		HOSTLD=ld.lld \
+		HOSTCC=$HOSTCC \
+		HOSTCXX=$HOSTCXX \
+		HOSTAR=llvm-ar \
+		PATH=$C_PATH/bin:$PATH \
+		CROSS_COMPILE=$CC_64 \
+		CC_COMPAT=$CC_COMPAT \
+		CROSS_COMPILE_COMPAT=$CC_32 \
+		LD_LIBRARY_PATH=$C_PATH/lib:$LD_LIBRARY_PATH \
+		2>&1 | tee log.txt
 }
 
 # Functions to send messages/files to telegram
 tg_post_msg() {
 	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$CHATID" \
-	-d "disable_web_page_preview=true" \
-	-d "parse_mode=html" \
-	-d text="$1"
+		-d "disable_web_page_preview=true" \
+		-d "parse_mode=html" \
+		-d text="$1"
 }
 
 tg_post_build() {
@@ -136,10 +140,10 @@ tg_post_build() {
 
 	#Show the Checksum alongwith caption
 	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
-	-F chat_id="$CHATID"  \
-	-F "disable_web_page_preview=true" \
-	-F "parse_mode=html" \
-	-F caption="$2 | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
+		-F chat_id="$CHATID" \
+		-F "disable_web_page_preview=true" \
+		-F "parse_mode=html" \
+		-F caption="$2 | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
 }
 
 # Cleanup the build environment
@@ -153,7 +157,7 @@ build() {
 	compile
 }
 
-# BUILD-START
+# Let the compilation begin
 compile() {
 	CFLAG=$DFCF
 	muke
@@ -167,7 +171,7 @@ compile() {
 	CFLAG=-j$(nproc --all)
 	muke
 
-# BUILD-END
+	# Compilation ends
 	BUILD_END=$(date +"%s")
 
 	echo -e "$B"
@@ -182,15 +186,15 @@ check() {
 		if [[ -d $ZIP_DIR ]]; then
 			zip_ak
 		else
-			error 'Anykernel not present cannot zip'
+			error 'Anykernel is not present, cannot zip'
 		fi
 	else
 		tg_post_build "log.txt" "Build failed!!"
-		error 'Kernel image not found'
+		error 'Kernel image not found!!'
 	fi
 }
 
-# Pack the zip
+# Pack the image-gz.dtb using AnyKernel3
 zip_ak() {
 	source work/.config
 
@@ -198,46 +202,48 @@ zip_ak() {
 	FDEVICE2=${DEVICE2^^}
 	KNAME=$(echo "$CONFIG_LOCALVERSION" | cut -c 2-)
 
-	if [[ "$CONFIG_LTO_CLANG_THIN" != "y" && "$CONFIG_LTO_CLANG_FULL" == "y" ]]; then
+	if [ "$CONFIG_LTO_CLANG_THIN" != "y" ] && [ "$CONFIG_LTO_CLANG_FULL" == "y" ]; then
 		VARIANT='FULL_LTO'
-	elif [[ "$CONFIG_LTO_CLANG_THIN" == "y" && "$CONFIG_LTO_CLANG_FULL" == "y" ]]; then
+	elif [ "$CONFIG_LTO_CLANG_THIN" == "y" ] && [ "$CONFIG_LTO_CLANG_FULL" == "y" ]; then
 		VARIANT='THIN_LTO'
 	else
 		VARIANT='NON_LTO'
 	fi
 
-	case $CAM_LIB in 
-		1)
-		   CAM=NEWCAM
+	case $CAM_LIB in
+	1)
+		CAM=NEWCAM
 		;;
-		2)
-		   CAM=OLDCAM
+	2)
+		CAM=OLDCAM
 		;;
-		3)
-		   CAM=OSSCAM
+	3)
+		CAM=OSSCAM
 		;;
 	esac
 
-	case $HAPTICS in 
-		1)
-		   HAPTIC=QPNP
+	case $HAPTICS in
+	1)
+		HAPTIC=QPNP
 		;;
-		2)
-		   HAPTIC=QTI
+	2)
+		HAPTIC=QTI
 		;;
 	esac
 
 	cp $KERNEL_DIR/work/arch/arm64/boot/Image.gz-dtb $ZIP_DIR/
 
+# Post the log after a successful build
 	tg_post_build "log.txt" "Compiled kernel successfully!!"
 
 	cd $ZIP_DIR
 
-	FINAL_ZIP="$KNAME-$CAM-$HAPTIC-$FDEVICE2-$FDEVICE-`date +"%H%M"`"
+	FINAL_ZIP="$KNAME-$CAM-$HAPTIC-$FDEVICE2-$FDEVICE-$(date +"%H%M")"
 	zip -r9 "$FINAL_ZIP".zip * -x README.md LICENSE FUNDING.yml *placeholder zipsigner*
 	java -jar zipsigner* "$FINAL_ZIP.zip" "$FINAL_ZIP-signed.zip"
 	FINAL_ZIP="$FINAL_ZIP-signed.zip"
 
+# Post the kernel zip
 	tg_post_build "$FINAL_ZIP" "${CAM}+${HAPTIC}"
 
 	cd $KERNEL_DIR
@@ -248,20 +254,20 @@ zip_ak() {
 	COMMIT_HASH=$(git rev-parse --short HEAD)
 
 # Print the build information
-tg_post_msg "
-=========Scarlet-X Kernel=========
-  Compiler: <code>$CONFIG_CC_VERSION_TEXT</code>
-  Linux Version: <code>$KV</code>
-  Maintainer: <code>$USER</code>
-  Device: <code>$NAME</code>
-  Codename: <code>$DEVICE</code>
-  Zipname: <code>$FINAL_ZIP</code>
-  Variant: <code>$VARIANT</code>
-  Build Date: <code>$(date +"%Y-%m-%d %H:%M")</code>
-  Build Duration: <code>$(($DIFF / 60)).$(($DIFF % 60)) mins</code>
-  Last Commit Name: <code>$COMMIT_NAME</code>
-  Last Commit Hash: <code>$COMMIT_HASH</code>
-"
+	tg_post_msg "
+	=========Scarlet-X Kernel=========
+	Compiler: <code>$CONFIG_CC_VERSION_TEXT</code>
+	Linux Version: <code>$KV</code>
+	Maintainer: <code>$USER</code>
+	Device: <code>$NAME</code>
+	Codename: <code>$DEVICE</code>
+	Zipname: <code>$FINAL_ZIP</code>
+	Variant: <code>$VARIANT</code>
+	Build Date: <code>$(date +"%Y-%m-%d %H:%M")</code>
+	Build Duration: <code>$(($DIFF / 60)).$(($DIFF % 60)) mins</code>
+	Last Commit Name: <code>$COMMIT_NAME</code>
+	Last Commit Hash: <code>$COMMIT_HASH</code>
+	"
 	exit 0
 }
-	pass
+pass
