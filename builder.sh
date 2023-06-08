2b8d082855c77cee8d143e736cdd30584ed2d8fc
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-06-09 03:01:10 +0600

    builder: Switch to `rebase-2` branch
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- builder.sh
+++ builder.sh
@@ -21,7 +21,7 @@ cd ../..
 gsc https://github.com/Tashar02/AnyKernel3.git AnyKernel3
 
 # Clone Kernel Source
-gsc https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b rebase Kernel
+gsc https://github.com/Atom-X-Devs/android_kernel_xiaomi_scarlet.git -b rebase-2 Kernel
 
 # Setup Scripts
 mv scarlet.sh Kernel/scarlet.sh
