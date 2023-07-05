80fd1f35c23be7a0582d27cdb3c2312d13a5b310
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-07-06 02:01:40 +0600

    builder: Don't clone GCC
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- builder.sh
+++ builder.sh
@@ -10,7 +10,7 @@ mkdir toolchains
 
 # Clone GCC
 #gsc https://github.com/cyberknight777/gcc-arm64.git -b master toolchains/gcc64
-gsc https://github.com/mvaisakh/gcc-arm.git -b gcc-master toolchains/gcc32
+#gsc https://github.com/mvaisakh/gcc-arm.git -b gcc-master toolchains/gcc32
 
 # Clone CLANG
 mkdir toolchains/clang && cd toolchains/clang
